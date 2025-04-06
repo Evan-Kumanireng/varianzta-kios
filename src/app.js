1be422673aac387b3e8e3e9edf7dc424b4ffdbcb
devanthmtc <stefanusbaokumanireng@gmail.com>
2025-04-06 22:13:46 +0800

    penambahan fitur pesan otomatis

--- src/app.js
+++ src/app.js
@@ -8,4 +8,112 @@ document.addEventListener("alpine:init", () => {
       { id: 5, name: "Top Kopi Gula", img: "5.jpg", price: 5000 },
     ],
   }));
+
+  Alpine.store("cart", {
+    items: [],
+    total: 0,
+    quantity: 0,
+    add(newItem) {
+      // cek duplikasi barang di cart
+      const cartItem = this.items.find((item) => item.id === newItem.id);
+
+      // jika belum ada barang atau keranjang kosong
+      if (!cartItem) {
+        this.items.push({ ...newItem, quantity: 1, total: newItem.price });
+        this.quantity++;
+        this.total += newItem.price;
+      } else {
+        // jika barang sudah ada, rumus cek apakah barang beda atau sama dengan yang ada di cart
+        this.items = this.items.map((item) => {
+          // jika barang beda
+          if (item.id !== newItem.id) {
+            return item;
+          } else {
+            // jika barang sudah ada, tambah kuantiti dan totalnya
+            item.quantity++;
+            item.total = item.price * item.quantity;
+            this.quantity++;
+            this.total += item.price;
+            return item;
+          }
+        });
+      }
+    },
+    remove(id) {
+      // ambil item yang mau diremove berdasarkan id-nya
+      const cartItem = this.items.find((item) => item.id === id);
+
+      // jika item lebih dari 1
+      if (cartItem.quantity > 1) {
+        // telusuri 11
+        this.items = this.items.map((item) => {
+          // jika bukan barang yang diklik
+          if (item.id !== id) {
+            return item;
+          } else {
+            item.quantity--;
+            item.total = item.price * item.quantity;
+            this.quantity--;
+            this.total -= item.price;
+            return item;
+          }
+        });
+      } else if (cartItem.quantity === 1) {
+        // jika barangnya sisa 1
+        this.items = this.items.filter((item) => item.id !== id);
+        this.quantity--;
+        this.total -= cartItem.price;
+      }
+    },
+  });
 });
+
+// Form Validasi Pembayaran
+const checkoutButton = document.querySelector(".checkout-button");
+checkoutButton.disabled = true;
+
+const form = document.querySelector("#checkoutForm");
+form.addEventListener("keyup", function () {
+  for (let i = 0; i < form.elements.length; i++) {
+    if (form.elements[i].value.length !== 0) {
+      checkoutButton.classList.remove("disabled");
+      checkoutButton.classList.add("disabled");
+    } else {
+      return false;
+    }
+  }
+  checkoutButton.disabled = false;
+  checkoutButton.classList.remove("disabled");
+});
+
+// kirim data ketika tombol bayar diklik
+checkoutButton.addEventListener("click", function (e) {
+  e.preventDefault();
+  const formData = new FormData(form);
+  const data = new URLSearchParams(formData);
+  const objData = Object.fromEntries(data);
+  const message = formatMessage(objData);
+  window.open("http://wa.me/6281353233825?text=" + encodeURIComponent(message));
+});
+
+//format pesan di whatsapp devanth
+const formatMessage = (obj) => {
+  return `Data Pembeli
+  Nama: ${obj.name}
+  Email: ${obj.email}
+  No HP: ${obj.phone}
+  Data Pesanan: ${JSON.parse(obj.items).map(
+    (item) => `${item.name} (${item.quantity} x ${rupiah(item.total)}) \n`
+  )} 
+  TOTAL: ${rupiah(obj.total)}
+  Terima kasih`;
+};
+
+// konversi Mata Uang Ke Rupiah Devanth Design
+const rupiah = (number) => {
+  return new Intl.NumberFormat("id-ID", {
+    style: "currency",
+    currency: "IDR",
+    minimumFractionDigits: 0,
+  }).format(number);
+};
